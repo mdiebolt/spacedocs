1fe231b6f290fa5a9f4f43bbbe4134d6afbaaa30
Matt Diebolt <mdiebolt@gmail.com>
2012-05-14 15:50:10 -0700

    more process_data refactoring

--- lib/spacedocs.rb
+++ lib/spacedocs.rb
@@ -3,11 +3,9 @@ require 'haml'
 require 'json'
 
 module Spacedocs
-  DIR = Dir.pwd
-
   class << self
     def partial(template_name, locals={})
-      Tilt.new("#{DIR}/source/#{template_name}").render self, locals
+      Tilt.new("source/#{template_name}").render self, locals
     end
 
     def doc
@@ -46,16 +44,34 @@ module Spacedocs
       output
     end
 
-    def method_of_data(tags)
+    def tags_named(name, tags)
       tags.map do |tag|
-        tag['string'] if tag['type'] == 'methodOf'
+        tag['string'] if tag['type'] == name
       end.compact
     end
 
+    def method_of_data(tags)
+      tags_named('methodOf', tags)
+    end
+
     def name_data(tags)
-      tags.map do |tag|
-        tag['string'] if tag['type'] == 'name'
-      end.compact
+      tags_named('name', tags)
+    end
+
+    def params_data(tags)
+      params = {}
+
+      tags.each do |tag|
+        if tag['type'] == 'param'
+          params["#{tag['name'].gsub(/[\[\]]/, '')}"] = {
+            "type" => tag['types'].join(', '),
+            "description" => tag['description'],
+            "optional" => tag['name'].start_with?('[')
+          }
+        end
+      end
+
+      return params
     end
 
     def returns_data(tags)
@@ -67,50 +83,51 @@ module Spacedocs
     end
 
     def see_data(tags)
-      tags.map do |tag|
-        tag['local'] if tag['type'] == 'see'
-      end.compact
+      tags_named('see', tags)
     end
 
     def format_class_name(name)
       name.end_with?('#') ? name : name + '.'
     end
 
+    def class_name_data(tags)
+      class_names = []
+
+      tags.each do |tag|
+        if tag['type'] == 'methodOf'
+          class_names << format_class_name(tag['string'])
+        end
+
+        if tag['type'] == 'constructor' || tag['type'] == 'namespace'
+          tags.each do |tag|
+            if tag['type'] == 'name'
+              class_names << format_class_name(tag['string'])
+            end
+          end
+        end
+      end
+
+      class_names.compact.flatten.uniq
+    end
+
     def process_data(json)
-      constructors = []
       class_names = []
       tags_list = []
       docs_data = []
       module_map = {}
 
       json.each do |item|
-        tags_list << item['tags']
+        tags = item['tags']
 
-        name = name_data(item['tags']).first
-        returns = returns_data(item['tags']).first
-        see = see_data(item['tags']).first
-        method_of = method_of_data(item['tags']).first
+        tags_list << tags
 
-        params = {}
+        name = name_data(tags).first
+        returns = returns_data(tags).first || {}
+        see = see_data(tags).first || ""
+        method_of = method_of_data(tags).first
+        params = params_data(tags)
 
-        (item['tags']).each do |tag|
-          if tag['type'] == 'param'
-            params["#{tag['name'].gsub(/[\[\]]/, '')}"] = {
-              "type" => tag['types'].join(', '),
-              "description" => tag['description'],
-              "optional" => tag['name'].start_with?('[')
-            }
-          end
-
-          if tag['type'] == 'methodOf'
-            source_class = format_class_name(tag['string'])
-            class_names << source_class
-          end
-
-          if tag['type'] == 'constructor' || tag['type'] == 'namespace'
-            constructors << item['tags']
-          end
-        end
+        class_names << class_name_data(tags)
 
         if name && method_of
           module_map[method_of] ||= {}
@@ -119,35 +136,13 @@ module Spacedocs
             "code_sample" => item['description']['body'],
             "source" => item['code'],
             "parameters" => params,
-            "returns" => returns || {},
-            "see" => see || ""
+            "returns" => returns,
+            "see" => see
           }
         end
       end
 
-      constructors.each do |tags|
-        tags.each do |tag|
-          if tag['type'] == 'name'
-            source_class = format_class_name(tag['string'])
-            class_names << source_class
-          end
-        end
-      end
-
-      class_names = class_names.uniq.sort
-
-      tags_list.each do |tags|
-        tags.each do |tag|
-          class_names.each do |source_class|
-            if tag['type'] == 'methodOf'
-              class_name = format_class_name(tag['string'])
-              if class_name == source_class
-                methods = methods_of(source_class, tags)
-              end
-            end
-          end
-        end
-      end
+      class_names = class_names.compact.flatten.uniq.sort
 
       class_names.each do |source_class|
         method_list = module_map[source_class] ? module_map[source_class].keys : []
